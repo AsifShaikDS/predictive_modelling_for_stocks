6f3d7164e9146011094cbc5cea95950902aeed62
Tejeswara Rao <tejeshdasari29@gmail.com>
2023-10-26 15:14:10 +0530

    Modified code and created Dockerfile

--- model.py
+++ model.py
@@ -1,90 +1,89 @@
-# Importing the Libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 from sklearn.preprocessing import MinMaxScaler
-from keras.layers import LSTM, Dense, Dropout
+from keras.layers import LSTM, Dense
 from sklearn.model_selection import TimeSeriesSplit
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import MinMaxScaler
-from sklearn import linear_model
 from keras.models import Sequential
-from keras.layers import Dense
-import keras.backend as K
-from keras.callbacks import EarlyStopping
-from keras.optimizers import Adam
-from keras.models import load_model
-from keras.layers import LSTM
-from keras.utils import plot_model
-import pydot
-import graphviz
+import yfinance as yf
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
-import tensorflow as tf
-# from keras.utils import vis_utils
+# Ask the user to enter the stock code
+stock_code = input("Enter the stock code (e.g., MSFT for Microsoft): ")
 
-#Get the Dataset
-df=pd.read_csv('MicrosoftStockData.csv',na_values=['null'],index_col='Date',parse_dates=True,infer_datetime_format=True)
-print(df.head())
+# Fetch stock data
+stock = yf.Ticker(stock_code)
+data = stock.history(period="10y", auto_adjust=False)
 
-#Print the shape of Dataframe  and Check for Null Values
+# Save data to a CSV file
+data.to_csv(f"{stock_code}StockData.csv")
+
+# Load the fetched dataset
+try:
+    df = pd.read_csv(f"{stock_code}StockData.csv", index_col='Date', parse_dates=True)
+    print("Dataset loaded successfully.")
+except FileNotFoundError:
+    print("File not found or invalid format. Please make sure the file exists and is in CSV format.")
+    exit(1)
+
+# Print the shape of Dataframe and Check for Null Values
 print("Dataframe Shape: ", df.shape)
 print("Null Value Present: ", df.isnull().values.any())
 print(df.columns)
-#Plot the True Adj Close Value
 
-# plt.plot(df['Close'])
-# plt.show()
-
-
-# df['Close'].plot()
-
-#Set Target Variable
+# Set Target Variable
 output_var = pd.DataFrame(df['Close'])
-#Selecting the Features
+# Selecting the Features
 features = ['Open', 'High', 'Low', 'Volume']
 
-#Scaling
+# Scaling
 scaler = MinMaxScaler()
 feature_transform = scaler.fit_transform(df[features])
-feature_transform= pd.DataFrame(columns=features, data=feature_transform, index=df.index)
+feature_transform = pd.DataFrame(columns=features, data=feature_transform, index=df.index)
 print(feature_transform.head())
 
+# Splitting to Training set and Test set
+timesplit = TimeSeriesSplit(n_splits=5)
 
-#Splitting to Training set and Test set
-timesplit= TimeSeriesSplit(n_splits=10)
+# Initialize lists to store true and predicted values for each fold
+true_values = []
+predicted_values = []
+
 for train_index, test_index in timesplit.split(feature_transform):
-        X_train, X_test = feature_transform[:len(train_index)], feature_transform[len(train_index): (len(train_index)+len(test_index))]
-        y_train, y_test = output_var[:len(train_index)].values.ravel(), output_var[len(train_index): (len(train_index)+len(test_index))].values.ravel()
+    X_train, X_test = feature_transform[:len(train_index)], feature_transform[len(train_index): (len(train_index)+len(test_index))]
+    y_train, y_test = output_var[:len(train_index)].values.ravel(), output_var[len(train_index): (len(train_index)+len(test_index))].values.ravel()
 
+    # Process the data for LSTM
+    trainX = np.array(X_train)
+    testX = np.array(X_test)
+    X_train = trainX.reshape(X_train.shape[0], 1, X_train.shape[1])
+    X_test = testX.reshape(X_test.shape[0], 1, X_test.shape[1])
 
-        #Process the data for LSTM
-        trainX =np.array(X_train)
-        testX =np.array(X_test)
-        X_train = trainX.reshape(X_train.shape[0], 1, X_train.shape[1])
-        X_test = testX.reshape(X_test.shape[0], 1, X_test.shape[1])
+    # Building the LSTM Model
+    lstm = Sequential()
+    lstm.add(LSTM(32, input_shape=(1, trainX.shape[1]), activation='relu', return_sequences=False))
+    lstm.add(Dense(1))
+    lstm.compile(loss='mean_squared_error', optimizer='adam')
 
+    history = lstm.fit(X_train, y_train, epochs=50, batch_size=16, verbose=0, shuffle=False)
 
-        #Building the LSTM Model
-        lstm = Sequential()
-        lstm.add(LSTM(32, input_shape=(1, trainX.shape[1]), activation='relu', return_sequences=False))
-        lstm.add(Dense(1))
-        lstm.compile(loss='mean_squared_error', optimizer='adam')
-        plot_model(lstm, show_shapes=True, show_layer_names=True)
+    y_pred = lstm.predict(X_test)
 
+    # Store the true and predicted values for each fold
+    true_values.append(y_test)
+    predicted_values.append(y_pred)
 
-        history=lstm.fit(X_train, y_train, epochs=100, batch_size=8, verbose=1, shuffle=False)
+# Display all the plots at once
+plt.figure(figsize=(12, 6))  # Adjust the figure size as needed
+for i in range(len(true_values)):
+    plt.subplot(2, 3, i + 1)  # Create subplots for each fold
+    plt.plot(true_values[i], label='True Value')
+    plt.plot(predicted_values[i], label='LSTM Value')
+    plt.title(f'Fold {i + 1}')
+    plt.xlabel('Time Scale')
+    plt.ylabel('Scaled USD')
+    plt.legend()
 
-        y_pred= lstm.predict(X_test)
-
-
-        #Predicted vs True Adj Close Value – LSTM
-        plt.plot(y_test, label='True Value')
-        plt.plot(y_pred, label='LSTM Value')
-        plt.title('Prediction by LSTM')
-        plt.xlabel('Time Scale')
-        plt.ylabel('Scaled USD')
-        plt.legend()
-        plt.show()
+plt.tight_layout()
+plt.show()
