f28503cbd0bbb1ebca2bb5d39a719c0312870332
AsifShaikDS <asif.ds.2024@gmail.com>
2023-12-09 00:31:30 +0530

    "test "

--- stockPrediction/tests/test_main.py
+++ stockPrediction/tests/test_main.py
@@ -1,56 +1,27 @@
 import unittest
-from unittest.mock import patch
-from datetime import date
-from io import StringIO
-from contextlib import redirect_stdout
-import streamlit as st
-import yfinance as yf
-from prophet import Prophet
-from prophet.plot import plot_plotly
-from plotly import graph_objs as go
 
-# Paste your provided code here
+# Function to be tested
+def add_numbers(a, b):
+    return a + b
 
-class TestStockForecastApp(unittest.TestCase):
+# Test case class
+class TestAddNumbers(unittest.TestCase):
+    def test_add_numbers(self):
+        # Test case 1: Positive numbers
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 5)
 
-    def setUp(self):
-        # Disable Streamlit output during tests
-        self.streamlit_patch = patch('streamlit.write')
-        self.streamlit_patch.start()
+        # Test case 2: Negative numbers
+        result = add_numbers(-2, -3)
+        self.assertEqual(result, -5)
 
-    def tearDown(self):
-        # Stop patching Streamlit
-        self.streamlit_patch.stop()
+        # Test case 3: Zero
+        result = add_numbers(0, 0)
+        self.assertEqual(result, 0)
 
-    @patch('streamlit.selectbox', return_value='GOOG')
-    @patch('streamlit.slider', return_value=5)
-    @patch('streamlit.text')
-    @patch('streamlit.write')
-    @patch('streamlit.plotly_chart')
-    @patch('streamlit.download')
-    @patch('streamlit.make_future_dataframe')
-    @patch('streamlit.fit')
-    @patch('streamlit.predict')
-    @patch('streamlit.plot_components')
-    def test_stock_forecast_app(self, mock_selectbox, mock_slider, mock_text,
-                                mock_write, mock_plotly_chart, mock_download,
-                                mock_make_future_dataframe, mock_fit, mock_predict,
-                                mock_plot_components):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            # Run the Streamlit app as if it's being executed from the command line
-            st.__setattr__('text', mock_text)
-            st.__setattr__('write', mock_write)
-            st.__setattr__('plotly_chart', mock_plotly_chart)
-
-            exec(open('main.py').read())
-
-            # Capture the Streamlit app's stdout for further assertions if needed
-            app_output = mock_stdout.getvalue()
-
-        # Add your assertions here if needed
-        self.assertIn("Forecast data", app_output)
-        self.assertIn("Forecast plot for 5 years", app_output)
-        self.assertIn("Forecast components", app_output)
+        # Test case 4: One positive, one negative
+        result = add_numbers(5, -3)
+        self.assertEqual(result, 2)
 
 if __name__ == '__main__':
     unittest.main()
